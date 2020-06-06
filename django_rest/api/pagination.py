c7c369db02be6ed9c111ff9d734c878cbc22da43
martinsmessias <messiasads2017@gmail.com>
2020-06-05 21:44:49 -0500

    Paginação customizada - sobrescrevendo métodos

--- django_rest/api/pagination.py
+++ django_rest/api/pagination.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class PaginacaoCustomizada(PageNumberPagination):
@@ -6,3 +7,13 @@ class PaginacaoCustomizada(PageNumberPagination):
     page_size_query_param = 'qtd'
     max_page_size = 100
     invalid_page_message = 'Mano???'
+
+    # Sobrescreve o método original
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'results': data
+        })
