09f77cc1fb0405ff9a468f840f3171161b102bc0
martinsmessias <messiasads2017@gmail.com>
2020-06-09 10:34:24 -0500

    AUTH - Django OAuth Toolkit

--- django_rest/api/admin.py
+++ django_rest/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tecnologia
+from .models import Tecnologia, Vaga
 
 admin.site.register(Tecnologia)
+admin.site.register(Vaga)
