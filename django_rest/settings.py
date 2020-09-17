c3a2df460638d0c708e565ad50ebe60d98888a67
MartinsMessias <messiasads2017@gmail.com>
2020-09-16 20:17:34 -0500

    Refatorando

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -22,7 +22,7 @@ except ModuleNotFoundError:
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "static"),
 )
 
 # Quick-start development settings - unsuitable for production
