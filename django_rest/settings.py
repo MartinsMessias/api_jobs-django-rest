7728c07b9b60f6ad00b73f4b8c9ca40a6a679592
MartinsMessias <messiasads2017@gmail.com>
2020-09-16 20:15:02 -0500

    Refatorando

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -20,7 +20,10 @@ except ModuleNotFoundError:
     pass
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "staticfiles"),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
