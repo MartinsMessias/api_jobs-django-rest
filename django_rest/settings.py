323b410e71a6790a8fc13b3488b6da0856aa0386
MartinsMessias <messiasads2017@gmail.com>
2020-09-16 19:45:36 -0500

    Refatorando

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
